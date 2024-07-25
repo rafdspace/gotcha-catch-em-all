cba8cb84310fcff88f97ecac4b147f4275962e26
idan <93237668+rafdspace@users.noreply.github.com>
2024-07-25 12:05:44 +0700

    feat: set pwa (#16)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import graphql from "@rollup/plugin-graphql";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
@@ -8,6 +9,44 @@ export default defineConfig({
       jsxImportSource: "@emotion/react",
     }),
     graphql(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon-180x180.png",
+        "pwa-64x64.png",
+      ],
+      manifest: {
+        name: "Pokemon Catch App",
+        short_name: "Pokemon Catch App",
+        description: "Pokemon Catch Progressive Web App!",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
   ],
   server: {
     open: true,
