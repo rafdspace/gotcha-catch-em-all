081cbc9ec883314df353c9bf015669dfff1ac798
idan <93237668+rafdspace@users.noreply.github.com>
2024-10-28 14:54:51 +0700

    feat: add PWA trigger button (#22)
    * feat: add PWA trigger button
    
    * feat: update config
    
    * fix: check error
    
    ---------
    
    Co-authored-by: Rafdian <rafdian.ramadhan@Rafdians-MacBook-Pro.local>

---
+++ src/usecases/usePWAInstall.ts
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+interface BeforeInstallPromptEvent extends Event {
+  prompt: () => Promise<void>;
+  userChoice: Promise<{ outcome: "accepted" | "dismissed" }>;
+}
+
+export const usePWAInstall = () => {
+  const [deferredPrompt, setDeferredPrompt] =
+    useState<BeforeInstallPromptEvent | null>(null);
+  const [isAppInstallable, setIsAppInstallable] = useState(false);
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e: Event) => {
+      e.preventDefault();
+      setDeferredPrompt(e as BeforeInstallPromptEvent);
+      setIsAppInstallable(true);
+    };
+
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    return () =>
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+  }, []);
+
+  const installPWA = async () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      const choiceResult = await deferredPrompt.userChoice;
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the install prompt");
+      } else {
+        console.log("User dismissed the install prompt");
+      }
+      setDeferredPrompt(null);
+      setIsAppInstallable(false);
+    }
+  };
+
+  return { isAppInstallable, installPWA };
+};
